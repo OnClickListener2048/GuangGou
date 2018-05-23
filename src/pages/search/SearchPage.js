a0583d837b718c62bf9976eb53a97fb8d001eada
wangxin <55997027@qq.com>
2018-05-23 23:37:09 +0800

    完成搜索列表  todo 搜索列表点击逻辑 刷新及空页面逻辑

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -6,8 +6,12 @@ import {
     , TextInput
     , TouchableOpacity
     , Dimensions
+    , FlatList
+    , ActivityIndicator
+    , AsyncStorage
 } from "react-native";
 import {SCREEN_HEIGHT, SCREEN_WIDTH} from "../../utils/Constant";
+import MainPageItem from "../main/MainPageItem";
 
 const {width = SCREEN_WIDTH, height = SCREEN_HEIGHT} = Dimensions.get("window");
 
@@ -29,7 +33,63 @@ export default class SearchPage extends Component {
     };
 
 
+    constructor(props) {
+        super(props)
+
+        this.changeText = "";
+
+
+    }
+
+    state = {
+        data: [],
+        refreshing: false,
+        loaded: true,
+        loadMore: false,
+    };
+
+    loadData() {
+        alert(this);
+        if (!this.changeText) return;
+
+        let params = {
+            "q": this.changeText,
+        };
+
+        this.setState({
+            loaded: true,
+        });
+        HTTPBase.get("http://guangdiu.com/api/getresult.php", params)
+            .then(response => {
+                this.setState({
+                    data: response.data,
+                });
+
+                let searchLastID = response.data[response.data.length - 1].id;
+                AsyncStorage.setItem('searchLastID', searchLastID.toString());
+            })
+
+    }
+
+    popupHome() {
+        this.props.navigation.goBack();
+    }
+
+    onItemPress = () => {
+
+    };
+
+    _onEndReached = () => {
+
+    };
+
+    _onRefresh = () => {
+
+    };
+
     render() {
+        const {loaded, loadMore, refreshing, data} = this.state;
+        alert(data);
         return (
             <View style={SearchStyles.container}>
                 <View style={SearchStyles.searchArea}>
@@ -43,13 +103,15 @@ export default class SearchPage extends Component {
                                    placeholderTextColor='gray'
                                    autoFocus={true}
                                    clearButtonMode="while-editing"
-                                   onChangeText={(text) => {this.changeText = text}}
-                                   onEndEditing={() => this.loadData()}
+                                   onChangeText={(text) => {
+                                       this.changeText = text
+                                   }}
+                                   onEndEditing={() => this.loadData(this)}
                                    underlineColorAndroid={'transparent'}
                         />
                     </View>
                     <View style={SearchStyles.rightCancelArea}>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={() => this.popupHome(this)}>
                             <Text style={SearchStyles.rightCancel}>
                                 取消
                             </Text>
@@ -57,6 +119,30 @@ export default class SearchPage extends Component {
                     </View>
                 </View>
 
+                {loaded ?  <View style={SearchStyles.container}>
+                    <FlatList contentContainerStyle={SearchStyles.root}
+                              data={data}
+                              renderItem={({item}) => (<MainPageItem
+                                  image={item.image}
+                                  title={item.title}
+                                  name={item.mall}
+                                  id={item.id}
+                                  onPress={(id) => this.onItemPress(id)}
+                              />)}
+                              keyExtractor={(item) => item.id}
+                              ListFooterComponent={() => {
+                                  return loadMore ? <ActivityIndicator size="large" style={{
+                                      marginTop: 5,
+                                      marginBottom: 5
+                                  }}/> : null;
+                              }}
+                              onEndReached={this._onEndReached}
+                              onRefresh={this._onRefresh}
+                              refreshing={refreshing}
+                              onEndReachedThreshold={0.1}
+                    />
+
+                </View> : <ActivityIndicator size="large" style={{marginTop: 15}}/>}
 
             </View>
         );
@@ -68,6 +154,10 @@ export default class SearchPage extends Component {
 const SearchStyles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+
+    root:{
+
     },
 
     searchArea: {
@@ -109,7 +199,7 @@ const SearchStyles = StyleSheet.create({
 
     rightCancelArea: {
         flex: 1,
-        alignItems:"center",
+        alignItems: "center",
     },
 
 
